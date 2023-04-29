d126119e6db936828928b968ecb92194ea8eb6e2
andrii_ivantsiv <5127372+andrii-ivantsiv@users.noreply.github.com>
2023-04-29 10:37:32 +0200

    Init

---
+++ new-features/README.md
@@ -0,0 +1,1 @@
+# [java 17 new features](https://openjdk.org/projects/jdk/17/) 
---
+++ new-features/src/main/java/org/example/record/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * @see <a href="https://openjdk.org/jeps/395">JEP 395: Records</a>
+ */
+package org.example.record;
